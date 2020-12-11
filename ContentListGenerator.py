79707685541847dec6b3e485c55c0e06b8f6d885
Tom Reed <t.reed1104@gmail.com>
2020-12-11 10:04:55 +0000

    Setup terminal arguments for the script

--- ContentListGenerator.py
+++ ContentListGenerator.py
@@ -1,5 +1,8 @@
 def main():
-    return
+    # Register the program's command line arguments
+    parser = ArgumentParser(description='Markdown contents list generator.')
+    parser.add_argument("-i", "--input", dest="input", help="Specify the markdown file to generate the content list from.", type=str, required=True)
+    arguments = parser.parse_args()
 
 ## App entry
 if __name__ == '__main__':
