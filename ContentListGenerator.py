8e9425fefa3ab40444fbac3dffeb69f8bbdd22a5
Tom Reed <t.reed1104@gmail.com>
2020-12-11 10:05:12 +0000

    Output folder generation

--- ContentListGenerator.py
+++ ContentListGenerator.py
@@ -4,6 +4,14 @@ def main():
     parser.add_argument("-i", "--input", dest="input", help="Specify the markdown file to generate the content list from.", type=str, required=True)
     arguments = parser.parse_args()
 
+    # Program runtime
+    print("---------------------------\nMarkdown File Content List Generator\n---------------------------")
+    outputDirectoryName = "output"
+    try:
+        os.makedirs(outputDirectoryName)
+        print(">> Creating the output directory")
+    except FileExistsError:
+        pass
 ## App entry
 if __name__ == '__main__':
     main()
